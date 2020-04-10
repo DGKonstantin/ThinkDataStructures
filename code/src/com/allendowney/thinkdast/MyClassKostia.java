48a9c2f823ed19002ee39cffb84657abd3024bc6
DGKonstantin <kot.maloi@mail.ru>
2020-04-10 19:33:51 +0300

    MyClassKostia

---
+++ code/src/com/allendowney/thinkdast/MyClassKostia.java
@@ -0,0 +1,34 @@
+package com.allendowney.thinkdast;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+
+public class MyClassKostia {
+    public static void main(String[] args) throws IOException {
+        String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
+
+        Connection conn = Jsoup.connect(url);
+        Document document = conn.get();
+
+        Element element = document.getElementById("mw-content-text");
+        Elements paragrarhs = element.select("p");
+
+
+        Element para = paragrarhs.get(0);
+
+        System.out.println(para);
+        Iterable<Node> iter = new WikiNodeIterable(para);
+        for(Node node : iter){
+            if (node instanceof TextNode)
+                System.out.println(node);
+        }
+    }
+}
